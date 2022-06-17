7d88ebf51b421d361f53f933d7fb259206a35fce
Giovanna Bellonia <33262563+gilouzi@users.noreply.github.com>
2022-06-17 16:54:43 -0300

    Creating basic calculator with tests

---
+++ calculator.py
@@ -0,0 +1,17 @@
+from unittest import result
+
+
+class Calculator:
+  
+  def __init__(self, save_result_on_memory = False):
+    self._result = 0
+    self._save_result_on_memory = save_result_on_memory
+
+  def add(self, number):
+    self._result += number
+
+  def get_result(self):
+    result = self._result
+    if not self._save_result_on_memory:
+      self._result = 0
+    return result
---
+++ test_calculator.py
@@ -0,0 +1,31 @@
+import unittest
+from calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+
+  def test_result_is_zero_on_initialization(self):
+    calculator = Calculator()
+    self.assertEqual(0, calculator.get_result())
+
+  def test_add_one_number(self):
+    calculator = Calculator()
+    calculator.add(5)
+    self.assertEqual(5, calculator.get_result())
+
+  def test_add_two_numbers(self):
+    calculator = Calculator()
+    calculator.add(5)
+    calculator.add(3)
+    self.assertEqual(8, calculator.get_result())
+
+  def test_result_is_zero_after_getting_result(self):
+    calculator = Calculator()
+    calculator.add(5)
+    calculator.get_result()
+    self.assertEqual(0, calculator.get_result())
+
+  def test_result_is_saved_after_getting_result(self):
+    calculator = Calculator(True)
+    calculator.add(5)
+    calculator.get_result()
+    self.assertEqual(5, calculator.get_result())
